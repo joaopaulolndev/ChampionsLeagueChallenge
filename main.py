c14c4561de17d3c2beacc0d1917105aa97917b5b
João Paulo Leite Nascimento <joaopauloln7@gmail.com>
2019-02-01 13:50:08 -0200

    Update main.py

--- main.py
+++ main.py
@@ -192,4 +192,9 @@ async def final(request, season, team1, team2):
 
 # Run App
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    #app.run(host="0.0.0.0", port=8000)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000)),
+        workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+        debug=bool(os.environ.get('DEBUG', '')))
